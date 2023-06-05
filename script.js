38a9a6fc49e2d1eca7cd5ebf4e2c69bcabfa78bd
Fábio <fabiosantana5057@gmail.com>
2023-06-05 18:46:10 -0300

    alteração de imagens

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function toggleMode() {
     const img = document.querySelector("#profile img")
     //substituir img
     if(html.classList.contains('light')) {
-        img.setAttribute('src', './assets/avatar.png')
-    } else {
         img.setAttribute('src', './assets/avatar-light.png')
+    } else {
+        img.setAttribute('src', './assets/avatar.png')
     }
 
 }
